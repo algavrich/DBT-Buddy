a664379f4333b408f10aa169b234b167b28f4d4c
Lucy Gavrich <annalgav@gmail.com>
2022-09-12 15:19:07 -0700

    Created basic model for diary-card-app database

---
+++ model.py
@@ -0,0 +1,70 @@
+"""Models for diary card app."""
+
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+
+class User(db.Model):
+    """Model for a user."""
+
+    __tablename__ = "users"
+
+    user_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    email = db.Column(db.String(50), nullable=False)
+    password = db.Column(db.String(25), nullable=False)
+    phone_number = db.Column(db.String(20))
+    entry_reminders = db.Column(db.Boolean, nullable=False)
+    med_tracking = db.Column(db.Boolean, nullable=False)
+    med_reminders = db.Column(db.Boolean, nullable=False)
+    emgcy_contact = db.Column(db.String(20))
+    fav_trend = db.Column(db.String(5))
+
+    diary_entries = db.relationship("DiaryEntry", back_populates="user")
+    med_entries = db.relationship("MedEntry", back_populates="med_entries")
+
+
+class DiaryEntry(db.Model):
+    """Model for a diary entry."""
+
+    __tablename__ = "diary_entries"
+
+    entry_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.user_id"), nullable=False)
+    datetime = db.Column(db.Datetime, nullable=False)
+    sad_score = db.Column(db.Integer, nullable=False)
+    angry_score = db.Column(db.Integer, nullable=False)
+    fear_score = db.Column(db.Integer, nullable=False)
+    happy_score = db.Column(db.Integer, nullable=False)
+    shame_score = db.Column(db.Integer, nullable=False)
+    urge_1 = db.Column(db.Integer, nullable=False)
+    urge_2 = db.Column(db.Integer, nullable=False)
+    urge_3 = db.Column(db.Integer, nullable=False)
+    action_1 = db.Column(db.Integer, nullable=False)
+    action_2 = db.Column(db.Integer, nullable=False)
+    skills_used = db.Column(db.Integer, nullable=False)
+
+    user = db.relationship("User", back_populates="diary_entries")
+
+
+class MedEntry(db.Model):
+    """Model for a med entry."""
+
+    __tablename__ = "med_entries"
+
+    entry_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.user_id"), nullable=False)
+    datetime = db.Column(db.Datetime, nullable=False)
+
+    user = db.relationship("User", back_populates="med_entries")
+
+
+
+def connect_to_db(app, db_name):
+    """Connect app to database."""
+
+    app.config["SQLALCHEMY_DATABASE_URI"] = f"postgresql:///{db_name}"
+    app.config["SQLALCHEMY_ECHO"] = True
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    db.app = app
+    db.init_app(app)
