7b25109a04a082b3923260263b02a79c3847fda9
Lucy Gavrich <annalgav@gmail.com>
2022-09-15 18:47:53 -0700

    Logic for toggling between viewing and editing today's entry

---
+++ static/js/dashboard.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const editTodayButton = document.querySelector('#edit-today');
+const todayEntry = document.querySelector('.diary-day');
+const editTodayForm = document.querySelector('#edit-today-form');
+let clicked = false
+
+editTodayButton.addEventListener('click', () => {
+    if (clicked) {
+        clicked = false;
+        todayEntry.style.display = 'block';
+        editTodayForm.style.display = 'none';
+        editTodayButton.innerHTML = 'Edit Today\'s Entry';
+    } else {
+        clicked = true;
+        todayEntry.style.display = 'none';
+        editTodayForm.style.display = 'block';
+        editTodayButton.innerHTML = 'Cancel';
+    }
+});
