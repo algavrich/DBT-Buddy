18d14e2d5f4333cd40bacec00d7832d8cc418830
Lucy Gavrich <annalgav@gmail.com>
2022-09-16 20:29:41 -0700

    Did some print debugging, still having issue with selecting all action inputs

--- static/js/dashboard.js
+++ static/js/dashboard.js
@@ -18,3 +18,59 @@ editTodayButton.addEventListener('click', () => {
         editTodayButton.innerHTML = 'Cancel';
     }
 });
+
+editTodayForm.addEventListener('submit', (evt) => {
+    evt.preventDefault();
+
+    const action1Buttons = document.querySelectorAll('input[name="action-1"]');
+    let action1 = null;
+    for (const button of action1Buttons) {
+        if (button.checked) {
+            action1 = button.value;
+            break;
+        }
+    }
+
+    const action2Buttons = document.querySelectorAll('input[name="action-2"]');
+    let action2 = null;
+    for (const button of action2Buttons) {
+        if (button.checked) {
+            action2 = button.value;
+            break;
+        }
+    }
+
+    const formInputs = {
+        sad_score: document.querySelector('#sad').value,
+        angry_score: document.querySelector('#angry').value,
+        fear_score: document.querySelector('#fear').value,
+        happy_score: document.querySelector('#happy').value,
+        shame_score: document.querySelector('#shame').value,
+        urge1_score: document.querySelector('#urge-1').value,
+        urge2_score: document.querySelector('#urge-2').value,
+        urge3_score: document.querySelector('#urge-3').value,
+        action1_score: action1,
+        action1_score: action2,
+        used_skills: document.querySelector('#used-skills').value,
+    };
+
+    console.log(formInputs.action1_score);
+    console.log(formInputs.action2_score);
+
+    fetch('/api/update-today-entry', {
+        method: 'POST',
+        body: JSON.stringify(formInputs),
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    })
+    .then((res) => res.json())
+    .then((resData) => {
+        alert(resData.status);
+    });
+
+    clicked = false;
+    todayEntry.style.display = 'inline-block';
+    editTodayForm.style.display = 'none';
+    editTodayButton.innerHTML = 'Edit Today\'s Entry';
+});
