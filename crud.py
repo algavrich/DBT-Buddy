e291cfa7020453c8231920227fb71f3e36e9b0b6
Lucy Gavrich <annalgav@gmail.com>
2022-09-20 12:11:30 -0700

    Grouped functions together better, commented out med and fav_trend stuff, changed import rt statement from model

--- crud.py
+++ crud.py
@@ -1,7 +1,7 @@
 """CRUD functions."""
 
-from model import (db, connect_to_db, User, DiaryEntry, MedEntry, 
-                   UrgeEntry, ActionEntry, Urge, Action)
+from model import (db, connect_to_db, User, Urge, Action, DiaryEntry, 
+                   UrgeEntry, ActionEntry)
 from datetime import date, datetime, timedelta
 from sqlalchemy import cast, DATE
 from random import randint, choice
@@ -34,64 +34,6 @@ def create_user(
     )
 
     return user
-
-
-def create_diary_entry(
-    user_id, dt, sad_score, angry_score, fear_score,
-    happy_score, shame_score, skills_used):
-    """Create and return a new diary entry."""
-
-    d_entry = DiaryEntry(
-        user_id=user_id, 
-        dt=dt, 
-        sad_score=sad_score, 
-        angry_score=angry_score, 
-        fear_score=fear_score, 
-        happy_score=happy_score, 
-        shame_score=shame_score, 
-        skills_used=skills_used
-    )
-
-    return d_entry
-
-
-def create_med_entry(user_id, dt):
-    """Create and return a new med entry."""
-
-    m_entry = MedEntry(
-        user_id=user_id, 
-        dt=dt
-    )
-
-    return m_entry
-
-
-def create_urge_entry(urge_id, d_entry_id, user_id, dt, score):
-    """Create and return a new urge entry."""
-
-    u_entry = UrgeEntry(
-        urge_id=urge_id, 
-        d_entry_id=d_entry_id,
-        user_id=user_id, 
-        dt=dt,
-        score=score
-    )
-
-    return u_entry
-
-
-def create_action_entry(action_id, d_entry_id, user_id, dt, score):
-    """Create and return a new action entry."""
-
-    a_entry = ActionEntry(
-        action_id=action_id, 
-        d_entry_id=d_entry_id, 
-        user_id=user_id, 
-        dt=dt,
-        score=score
-    )
-
-    return a_entry
 
 
 def create_urge(user_id, description):
@@ -141,6 +83,64 @@ def create_account_helper(
     db.session.add_all(new_urges)
     db.session.add_all(new_actions)
     db.session.commit()
+
+
+def create_diary_entry(
+    user_id, dt, sad_score, angry_score, fear_score,
+    happy_score, shame_score, skills_used):
+    """Create and return a new diary entry."""
+
+    d_entry = DiaryEntry(
+        user_id=user_id, 
+        dt=dt, 
+        sad_score=sad_score, 
+        angry_score=angry_score, 
+        fear_score=fear_score, 
+        happy_score=happy_score, 
+        shame_score=shame_score, 
+        skills_used=skills_used
+    )
+
+    return d_entry
+
+
+# def create_med_entry(user_id, dt):
+#     """Create and return a new med entry."""
+
+#     m_entry = MedEntry(
+#         user_id=user_id, 
+#         dt=dt
+#     )
+
+#     return m_entry
+
+
+def create_urge_entry(urge_id, d_entry_id, user_id, dt, score):
+    """Create and return a new urge entry."""
+
+    u_entry = UrgeEntry(
+        urge_id=urge_id, 
+        d_entry_id=d_entry_id,
+        user_id=user_id, 
+        dt=dt,
+        score=score
+    )
+
+    return u_entry
+
+
+def create_action_entry(action_id, d_entry_id, user_id, dt, score):
+    """Create and return a new action entry."""
+
+    a_entry = ActionEntry(
+        action_id=action_id, 
+        d_entry_id=d_entry_id, 
+        user_id=user_id, 
+        dt=dt,
+        score=score
+    )
+
+    return a_entry
 
 
 def create_d_u_a_entries_helper(
@@ -210,18 +210,18 @@ def get_users_entry_reminders():
     return User.query.filter(User.entry_reminders == True).all()
 
 
-def get_users_med_reminders():
-    """Return all users who opted in to med reminders."""
+# def get_users_med_reminders():
+#     """Return all users who opted in to med reminders."""
 
-    return User.query.filter(User.med_reminders == True).all()
+#     return User.query.filter(User.med_reminders == True).all()
 
 
-def get_fav_trend_by_user_id(user_id):
-    """Return user's favorite trend when given their ID."""
+# def get_fav_trend_by_user_id(user_id):
+#     """Return user's favorite trend when given their ID."""
 
-    user = get_user_by_id(user_id)
+#     user = get_user_by_id(user_id)
 
-    return user.fav_trend
+#     return user.fav_trend
 
 
 def get_diary_entries_by_user_id(user_id):
@@ -232,12 +232,12 @@ def get_diary_entries_by_user_id(user_id):
     return user.diary_entries
 
 
-def get_med_entries_by_user_id(user_id):
-    """Return list of user's med entries when given their ID."""
+# def get_med_entries_by_user_id(user_id):
+#     """Return list of user's med entries when given their ID."""
 
-    user = get_user_by_id(user_id)
+#     user = get_user_by_id(user_id)
     
-    return user.med_entries
+#     return user.med_entries
 
 
 def get_urges_by_user_id(user_id):
