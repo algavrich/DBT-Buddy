3f3c1dcfe9adae3bf8504c5f5748e60ef7e96e96
Lucy Gavrich <annalgav@gmail.com>
2022-09-27 16:06:58 -0700

    Moved helper funcs here from server, wrote detailed docstrings and doctests for conversion to/from bool funcs

--- crud.py
+++ crud.py
@@ -6,16 +6,59 @@ from datetime import date, datetime, timedelta
 from sqlalchemy import cast, DATE
 from random import randint, choice
 import math
+from typing import TypeVar
+
+UA = TypeVar("UA", Urge, Action)
 
 # HELPER
 
-def convert_radio_to_bool(var):
-    """Convert a radio value to boolean."""
-
-    if var is None:
-        return False
+def convert_radio_to_bool(s: str) -> bool:
+    """Convert a radio value to boolean.
     
-    return var == "yes"
+    Takes in a string ('yes' or 'no'),
+    Returns True if string is 'yes',
+    Returns False if string is 'no'.
+
+    >>> convert_radio_to_bool('yes')
+    True
+    >>> convert_radio_to_bool('no')
+    False
+    >>> convert_radio_to_bool('foo')
+    False
+    >>> convert_radio_to_bool(3)
+    False
+
+    """
+    
+    if type(s) == str:
+        return s == "yes"
+
+    return False
+
+
+def convert_bool_to_y_n(value: bool) -> str:
+    """Convert a boolean value to yes or no.
+    
+    Takes in a boolean value,
+    Returns "yes" if value is True,
+    Returns "no" if value is False.
+
+    >>> convert_bool_to_y_n(True)
+    'yes'
+    >>> convert_bool_to_y_n(False)
+    'no'
+    >>> convert_bool_to_y_n('x')
+    'invalid input'
+
+    """
+    
+    if type(value) == bool:
+        if value:
+            return "yes"
+        
+        return "no"
+    
+    return "invalid input"
 
 # CREATE
 
@@ -326,6 +369,21 @@ def get_action_by_id(action_id):
     ).first()
 
 
+def get_descs_from_object_list(objects: list[UA]) -> list[str]:
+    """Get descriptions from list of user's custom Urge or Action object.
+    
+    Takes in a list of Urge or Action objects,
+    Returns a list of their descriptions.
+    """
+    
+    descriptions = []
+
+    for object in objects:
+        descriptions.append(object.description)
+
+    return descriptions
+
+
 def get_diary_entry_by_user_date(user_id, q_date):
     """Return a user's diary entry for a given date."""
 
@@ -356,6 +414,51 @@ def get_this_week_for_user(user_id):
     return get_given_week_for_user(
         user_id, (date.today() - timedelta(days=6))
     )
+
+
+def dict_for_day(entry):
+    """Create dictionary of values for given entry."""
+
+    return {
+        "date": datetime.strftime(entry.dt, "%A %d"),
+        "sad score": entry.sad_score,
+        "angry score": entry.angry_score,
+        "fear score": entry.fear_score,
+        "happy score": entry.happy_score,
+        "shame score": entry.shame_score,
+        "urge1 name": get_urge_desc_by_id(
+            entry.urge_entries[0].urge_id),
+        "urge2 name": get_urge_desc_by_id(
+            entry.urge_entries[1].urge_id),
+        "urge3 name": get_urge_desc_by_id(
+            entry.urge_entries[2].urge_id),
+        "urge1 score": entry.urge_entries[0].score,
+        "urge2 score": entry.urge_entries[1].score,
+        "urge3 score": entry.urge_entries[2].score,
+        "action1 name": get_action_desc_by_id(
+            entry.action_entries[0].action_id),
+        "action2 name": get_action_desc_by_id(
+            entry.action_entries[1].action_id),
+        "action1 score": convert_bool_to_y_n(
+            entry.action_entries[0].score),
+        "action2 score": convert_bool_to_y_n(
+            entry.action_entries[1].score),
+        "skills used": entry.skills_used
+    }
+
+
+def make_entries_jsonifiable(entries_as_objs):
+    """Turn list of dicts of entry objects into list of dicts of dicts."""
+
+    entries = []
+    for entry in entries_as_objs:
+        if entry is not None:
+            entry_contents = dict_for_day(entry)
+        else:
+            entry_contents = None
+        entries.append(entry_contents)
+
+    return entries
 
 
 def check_entry_today(user_id):
