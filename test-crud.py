0577eb6be95625c03d30e6f1b780eaaab1876484
Lucy Gavrich <annalgav@gmail.com>
2022-09-28 20:42:07 -0700

    Wrote InitializeEntriesTestCase class

--- test-crud.py
+++ test-crud.py
@@ -1,8 +1,10 @@
 """Unit tests for CRUD functions"""
 
-from model import connect_to_db, db, User, Urge, Action
+from model import (connect_to_db, db, User, Urge, Action, DiaryEntry,
+                   UrgeEntry, ActionEntry)
 import crud
 import unittest
+from datetime import datetime, date
 
 class ConversionTestCase(unittest.TestCase):
     """Unit tests for conversion functions."""
@@ -22,6 +24,7 @@ class ConversionTestCase(unittest.TestCase):
         self.assertEqual(crud.convert_bool_to_y_n(False), "no")
         self.assertEqual(crud.convert_bool_to_y_n("x"), "invalid input")
 
+
 class InitializeUserTestCase(unittest.TestCase):
     """Unit tests for functions that initialize a user."""
         
@@ -31,6 +34,18 @@ class InitializeUserTestCase(unittest.TestCase):
         connect_to_db(app, db_uri="postgresql:///testdb")
         db.create_all()
         crud.example_data()
+
+        test_user = User(
+            fname="Test",
+            email="test@test.test",
+            password="testpassword",
+            phone_number="1234567890",
+            entry_reminders=True,
+            med_tracking=True,
+            med_reminders=True
+        )
+        db.session.add(test_user)
+        db.session.commit()
 
     def tearDown(self):
         """Stuff to do after every test."""
@@ -48,20 +63,12 @@ class InitializeUserTestCase(unittest.TestCase):
             "1234567890", True, True, True
         )
         
-
         self.assertIsInstance(test_user, User)
         self.assertEqual(test_user.email, "test@test.test")
         self.assertEqual(test_user.med_reminders, True)
 
     def test_create_urge(self):
         """Test create_urge."""
-        
-        test_user = crud.create_user(
-            "Test", "test@test.test", "testpassword",
-            "1234567890", True, True, True
-        )
-        db.session.add(test_user)
-        db.session.commit()
 
         test_user = User.query.filter(
             User.email == "test@test.test"
@@ -74,13 +81,6 @@ class InitializeUserTestCase(unittest.TestCase):
 
     def test_create_action(self):
         """Test create_action."""
-
-        test_user = crud.create_user(
-            "Test", "test@test.test", "testpassword",
-            "1234567890", True, True, True
-        )
-        db.session.add(test_user)
-        db.session.commit()
 
         test_user = User.query.filter(
             User.email == "test@test.test"
@@ -95,12 +95,12 @@ class InitializeUserTestCase(unittest.TestCase):
         """Test create_account_helper."""
 
         crud.create_account_helper(
-            "Test", "test@test.test", "testpassword", "1234567890", True,
+            "Test2", "test2@test.test", "testpassword", "1234567890", True,
             True, True, "Urge1", "Urge2", "Urge3", "Action1", "Action2"
         )
 
         test_user = User.query.filter(
-            User.email == "test@test.test"
+            User.email == "test2@test.test"
         ).first()
 
         test_user_urges = []
@@ -112,13 +112,174 @@ class InitializeUserTestCase(unittest.TestCase):
             test_user_actions.append(action.description)
 
         self.assertIsInstance(test_user, User)
+        self.assertEqual(test_user.fname, "Test2")
         self.assertEqual(test_user.med_reminders, True)
         self.assertEqual(test_user_urges, ["Urge1", "Urge2", "Urge3"])
         self.assertEqual(test_user_actions, ["Action1", "Action2"])
 
 
+class InitializeEntriesTestCase(unittest.TestCase):
+    """Unit tests for functions that initialize a group of entry."""
+
+    def setUp(self):
+        """Stuff to do before every test."""
+
+        connect_to_db(app, db_uri="postgresql:///testdb")
+        db.create_all()
+        crud.example_data()
+
+        test_user = User(
+            fname="Test",
+            email="test@test.test",
+            password="testpassword",
+            phone_number="1234567890",
+            entry_reminders=True,
+            med_tracking=True,
+            med_reminders=True
+        )
+        db.session.add(test_user)
+        db.session.commit()
+
+        test_user = User.query.filter(
+            User.email == "test@test.test"
+        ).first()
+        new_urges = []
+        urge_names = ["Urge1", "Urge2", "Urge3"]
+        for urge_name in urge_names:
+            new_urges.append(
+                Urge(
+                    user_id=test_user.user_id,
+                    description=urge_name,
+                    active=True
+                )
+            )
+        new_actions = []
+        action_names = ["Action1", "Action2"]
+        for action_name in action_names:
+            new_actions.append(
+                Action(
+                    user_id=test_user.user_id,
+                    description=action_name,
+                    active=True
+                )
+            )
+        db.session.add_all(new_urges)
+        db.session.add_all(new_actions)
+
+        test_diary_entry = DiaryEntry(
+            user_id=test_user.user_id, 
+            dt=datetime.now(), 
+            sad_score=1, 
+            angry_score=2, 
+            fear_score=3, 
+            happy_score=4, 
+            shame_score=5, 
+            skills_used=7
+        )
+        db.session.add(test_diary_entry)
+        db.session.commit()
+        
+
+    def tearDown(self):
+        """Stuff to do after every test."""
+        
+        db.session.remove()
+        db.drop_all()
+        db.engine.dispose()
+
+    def test_create_diary_entry(self):
+        """Test create_diary_entry."""
+
+        test_user = User.query.filter(
+            User.email == "test@test.test"
+        ).first()
+
+        test_diary_entry = crud.create_diary_entry(
+            test_user.user_id, datetime.now(), 1, 2, 3, 4, 5, 7
+        )
+
+        self.assertIsInstance(test_diary_entry, DiaryEntry)
+        self.assertEqual(test_diary_entry.dt.date(), date.today())
+        self.assertEqual(test_diary_entry.angry_score, 2)
+        self.assertEqual(test_diary_entry.skills_used, 7)
+
+    def test_create_urge_entry(self):
+        """Test create_urge_entry."""
+
+        test_user = User.query.filter(
+            User.email == "test@test.test"
+        ).first()
+        test_diary_entry = DiaryEntry.query.filter(
+            DiaryEntry.user == test_user
+        ).first()
+
+        test_urge_entry = crud.create_urge_entry(
+            1, test_diary_entry.entry_id, test_user.user_id,
+            datetime.now(), 4
+        )
+
+        self.assertIsInstance(test_urge_entry, UrgeEntry)
+        self.assertEqual(
+            test_urge_entry.d_entry_id, test_diary_entry.entry_id
+        )
+        self.assertEqual(test_urge_entry.dt.date(), date.today())
+        self.assertEqual(test_urge_entry.score, 4)
+
+    def test_create_action_entry(self):
+        """Test create_action_entry."""
+
+        test_user = User.query.filter(
+            User.email == "test@test.test"
+        ).first()
+        test_diary_entry = DiaryEntry.query.filter(
+            DiaryEntry.user == test_user
+        ).first()
+
+        test_action_entry = crud.create_action_entry(
+            1, test_diary_entry.entry_id, test_user.user_id,
+            datetime.now(), 2
+        )
+
+        self.assertIsInstance(test_action_entry, ActionEntry)
+        self.assertEqual(
+            test_action_entry.d_entry_id, test_diary_entry.entry_id
+        )
+        self.assertEqual(test_action_entry.dt.date(), date.today())
+        self.assertEqual(test_action_entry.score, 2)
+
+    def test_create_d_u_a_entries_helper(self):
+        """Test create_d_u_a_entries_helper."""
+
+        test_user = User.query.filter(
+            User.email == "test@test.test"
+        ).first()
+
+        old_test_diary_entry = DiaryEntry.query.filter(
+            DiaryEntry.user == test_user
+        ).first()
+        db.session.delete(old_test_diary_entry)
+        db.session.commit()
+
+        crud.create_d_u_a_entries_helper(
+            test_user.user_id, 5, 4, 3, 2, 1, 1, 2, 3, True, False, 7
+        )
+
+        new_test_diary_entry = DiaryEntry.query.filter(
+            DiaryEntry.user == test_user
+        ).first()
+
+        self.assertIsInstance(new_test_diary_entry, DiaryEntry)
+        self.assertEqual(new_test_diary_entry.user_id, test_user.user_id)
+        self.assertEqual(new_test_diary_entry.sad_score, 5)
+        self.assertEqual(new_test_diary_entry.urge_entries[1].score, 2)
+        self.assertEqual(new_test_diary_entry.action_entries[1].score, False)
+        self.assertEqual(new_test_diary_entry.skills_used, 7)
+
+        
 
 
+
+    
 
 if __name__ == '__main__':
     from server import app
