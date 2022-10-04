cd2a63f558ddea5043ff854444717fb5fe778ba7
Lucy Gavrich <annalgav@gmail.com>
2022-10-03 17:21:13 -0700

    Copied logic from reminders to write med_reminders to send med reminders with Twilio. Calls corresponding CRUD functions and has a slightly different message. Additionally, added check to see if user is new, and only sends reminder if account is older than 24 hrs (along with other preexisting conditionals)

--- rem-check.py
+++ rem-check.py
@@ -16,7 +16,8 @@ def reminders():
 
     for user in users_entry_reminders:
         if (not crud.check_entry_past_24(user.user_id)
-            and not crud.check_for_reminder(user.user_id)):
+            and not crud.check_for_reminder(user.user_id)
+            and not crud.check_new_user(user.user_id)):
             message = " ".join([
                 f"Hi {user.fname}!",
                 "We noticed that you haven't made a",
@@ -32,7 +33,35 @@ def reminders():
             # Threading, queue
             crud.add_new_rem_to_db(user.user_id)
 
+
+def med_reminders():
+    """Check for users who need med reminders and send them."""
+
+    users_med_reminders = crud.get_users_med_reminders()
+
+    for user in users_med_reminders:
+        if (not crud.check_med_entry_past_24(user.user_id)
+            and not crud.check_for_med_reminder(user.user_id)
+            and not crud.check_new_user(user.user_id)):
+            message = " ".join([
+                f"Hi {user.fname}!",
+                "We noticed that you haven't recorded",
+                "taking your medications in the past day.",
+                "Please remember to take them. When you do,",
+                "head to link to log it!"
+            ])
+
+            client.api.account.messages.create(
+                to=f"+1{user.phone_number}",
+                from_="+19855895115",
+                body=message
+            )
+            # Threading, queue
+            crud.add_new_med_rem_to_db(user.user_id)
+
+
 if __name__ == '__main__':
     from server import app
     crud.connect_to_db(app, db_uri="postgresql:///test-db")
     reminders()
+    med_reminders()
